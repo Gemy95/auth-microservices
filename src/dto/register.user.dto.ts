3d951a6d26d6f817a3f0a227867519d7120b8541
Gemy95 <ali.gamal95880@gmail.com>
2023-12-21 09:46:19 +0200

    NF: update dto

--- src/dto/register.user.dto.ts
+++ src/dto/register.user.dto.ts
@@ -1,15 +1,20 @@
-import { IsString, IsNotEmpty, IsEmail } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class RegisterUserDto {
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-      name: string;
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+        name: string;
 
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  @IsEmail()
-      email: string;
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+    @IsEmail()
+        email: string;
+
+    @ApiProperty()
+    @IsString()
+    @IsNotEmpty()
+        password: string;
 }
